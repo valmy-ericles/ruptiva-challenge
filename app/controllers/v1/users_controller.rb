2ac49d1c4392279d6e1525895751767e095a1099
valmy <valmyericles@gmail.com>
2020-11-19 15:54:43 -0300

    user update ok

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,6 +1,6 @@
 module V1
   class UsersController < ApiController
-    before_action :load_user, only: %i[show]
+    before_action :load_user, only: %i[show update]
 
     def index
       @users = User.all
@@ -8,10 +8,28 @@ module V1
 
     def show; end
 
+    def update
+      @user.attributes = user_params
+      save_user!
+    end
+
     private
 
     def load_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      return {} unless params.key?(:user)
+
+      params.require(:user).permit(:id, :first_name, :last_name, :email, :role)
+    end
+
+    def save_user!
+      @user.save!
+      render :show
+    rescue StandardError
+      render_errors(fields: @user.errors.messages)
     end
   end
 end
--- spec/requests/v1/users/admin_requests_spec.rb
+++ spec/requests/v1/users/admin_requests_spec.rb
@@ -35,17 +35,49 @@ RSpec.describe 'V1::Users as :admin', type: :request do
       get url, headers: auth_header(user)
 
       expect(body_json['user']).to eq(user.as_json(only: %i[
-                                                      id
-                                                      first_name
-                                                      last_name
-                                                      email
-                                                      role
-                                                    ]))
+                                                     id
+                                                     first_name
+                                                     last_name
+                                                     email
+                                                     role
+                                                   ]))
     end
 
     it 'Returns success status' do
       get url, headers: auth_header(user)
       expect(response).to have_http_status(:ok)
     end
+  end
+
+  context 'PATCH /user' do
+    let(:user) { create(:user) }
+    let(:url) { "/v1/users/#{user.id}" }
+
+    context 'with valid params' do
+      let(:new_first_name) { 'Jhon' }
+      let(:new_last_name) { 'Doe' }
+
+      let(:user_params) { { user: { first_name: new_first_name, last_name: new_last_name } }.to_json }
+
+      it 'updates User' do
+        patch url, headers: auth_header(user), params: user_params
+        user.reload
+        expect(user.first_name).to eq new_first_name
+        expect(user.last_name).to eq new_last_name
+      end
+
+      it 'returns updated user' do
+        patch url, headers: auth_header(user), params: user_params
+        user.reload
+        expected_user = user.as_json(only: %i[id first_name last_name email role])
+        expect(body_json['user']).to eq expected_user
+      end
+
+      it 'returns success status' do
+        patch url, headers: auth_header(user), params: user_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
   end
 end
