1585d1aafbf08ab7bc8c988ed3fb49c711f635f8
valmy <valmyericles@gmail.com>
2020-11-19 13:41:47 -0300

    user tests and model ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true
+
+  enum role: { admin: 0, user: 1 }
 end
---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { '12345678' }
+    password_confirmation { '12345678' }
+    profile { :user }
+  end
+end
